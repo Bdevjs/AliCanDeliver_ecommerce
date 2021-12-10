9cc2b231146abc54e224fd7773fda34c2883e29f
mehedihassansovon <mehedi15-10712@diu.edu.bd>
2021-12-10 22:44:32 +0600

    database connect successfully

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,17 @@
 const app = require("./app");
 const dotenv = require("dotenv");
 
+//Database import file from database file 
+
+const connectDatabase = require("./config/database")
+
 //config
 
 dotenv.config({path:"backend/config/config.env"});
+
+
+//connecting to database
+connectDatabase()
 
 app.listen(process.env.PORT,()=>{
     console.log(`server is running on http://localhost:${process.env.PORT}`)
