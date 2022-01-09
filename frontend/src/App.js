ebad522e96515df19542b2346d6f87fff7d1e77e
mehedihassansovon <mehedi15-10712@diu.edu.bd>
2022-01-09 17:45:56 +0600

    done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import WebFont from "webfontloader"; //automatic download front [npm i webfontloader]
 import './App.css';
+import Footer from "./component/layout/Footer/Footer";
+import Header from "./component/layout/Header/Header.js";
 import Home from "./component/Home/Home";
 import ProductDetails from "./component/Product/ProductDetails";
 
